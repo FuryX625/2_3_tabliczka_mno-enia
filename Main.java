aa4b803cb8de11822d270037dd51866be6ceb9bb
FuryX625 <serafin.m625@gmail.com>
2022-04-10 21:33:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    int wiersz[]=new int[9];
+    int kolumna[]=new int[9];
+    for (int i=1;i<=9;i++)
+      {
+        wiersz[i]=i;
+        kolumna[i]=i;
+      }
+  }
+}
